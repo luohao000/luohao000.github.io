dc4fdbe80ddca654d383854767b8d44bc44418e3
luohao <luohao.chn@qq.com>
2026-02-17 22:29:15 +0800

    change organizationName and projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "luohao000", // Usually your GitHub org/user name.
+  projectName: "luohao000.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
 
