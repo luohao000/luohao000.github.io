fddcf32465898dee93d3c2ac4c7ac3e663bb7547
galdashen <galdashen@qq.com>
2026-02-25 14:37:21 +0800

    update

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,24 +10,24 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: "数学",
-    Svg: require("@site/static/img/subject-math.svg").default,
-    description: (
-      <>
-        数学，是研究数量、结构、变化、空间以及信息等概念的一门学科。数学是人类对事物的抽象结构与模式进行严格描述、推导的一种通用手段，可以应用于现实世界的任何问题，所有的数学对象本质上都是人为定义的。从这个意义上，数学属于形式科学，而不是自然科学。不同的数学家和哲学家对数学的确切范围和定义有一系列的看法。
-      </>
-    ),
-  },
-  {
-    title: "物理",
-    Svg: require("@site/static/img/subject-physics.svg").default,
-    description: (
-      <>
-        物理学，是研究物质最一般的运动规律和物质基本结构的学科。作为自然科学的带头学科，物理学研究大至宇宙，小至基本粒子等一切物质最基本的运动形式和规律，因此成为其他各自然科学学科的研究基础。物理学起始于伽利略和牛顿的年代，它已经成为一门有众多分支的基础科学。物理学是一门实验科学，也是一门崇尚理性、重视逻辑推理的科学。
-      </>
-    ),
-  },
+  // {
+  //   title: "数学",
+  //   Svg: require("@site/static/img/subject-math.svg").default,
+  //   description: (
+  //     <>
+  //       数学，是研究数量、结构、变化、空间以及信息等概念的一门学科。数学是人类对事物的抽象结构与模式进行严格描述、推导的一种通用手段，可以应用于现实世界的任何问题，所有的数学对象本质上都是人为定义的。从这个意义上，数学属于形式科学，而不是自然科学。不同的数学家和哲学家对数学的确切范围和定义有一系列的看法。
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: "物理",
+  //   Svg: require("@site/static/img/subject-physics.svg").default,
+  //   description: (
+  //     <>
+  //       物理学，是研究物质最一般的运动规律和物质基本结构的学科。作为自然科学的带头学科，物理学研究大至宇宙，小至基本粒子等一切物质最基本的运动形式和规律，因此成为其他各自然科学学科的研究基础。物理学起始于伽利略和牛顿的年代，它已经成为一门有众多分支的基础科学。物理学是一门实验科学，也是一门崇尚理性、重视逻辑推理的科学。
+  //     </>
+  //   ),
+  // },
   {
     title: "计算机",
     Svg: require("@site/static/img/subject-cs.svg").default,
@@ -57,7 +57,7 @@ export default function HomepageFeatures(): ReactNode {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className={clsx("row", styles.featuresRow)}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
